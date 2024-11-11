bf6c95412d248697b72b6b5287443718d880cee5
SongSang <941227_@naver.com>
2024-11-11 18:05:25 +0900

    🎉 :초기설정(scss, 폴더구조,샘플이미지 추가)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path'; // 1. path 선언
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')], // 2. sassOptions 옵션 추가
+  },
 };
 
 export default nextConfig;
