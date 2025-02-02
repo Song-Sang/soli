a158ed99eabda49c3e49afd740ad73e8a3941489
SongSang <941227_@naver.com>
2025-02-02 16:50:37 +0900

    ♻️ :workData 코드 리팩토링

---
+++ constant/WORKS_DATA.ts
@@ -0,0 +1,401 @@
+export const WORKS_DATA: WORKS_DATA = {
+  artwork: [
+    {
+      title: 'Embodied',
+      summary: 'What is embodied in me?',
+      category: 'performance, artwork',
+      content: ['Bachelor graduation work'],
+      role: ['Installation', 'Performance'],
+      people: {
+        crew: ['Soli Jang', 'Dongryoung Han'],
+        video: 'Hyunsung Choi',
+        photo: 'Xu Che',
+      },
+      schedule: [
+        {
+          date: '19, 20, 21 July 2024',
+          location: 'Universität der Künste Berlin, Berlin',
+        },
+      ],
+      images: Array.from(
+        { length: 6 },
+        (_, index) =>
+          `/images/Artwork&Performance/Embodied/Embodied${index + 1}.JPG`
+      ),
+    },
+  ],
+
+  performance: [
+    {
+      title: 'Denying the Dragon',
+      summary: 'Create new shape of dragons.',
+      category: 'performance',
+      content: [
+        '„Denying the Dragon“ has been unfolded from a myth where the dragon is forgotten or denied, transforming it into a narrative about the shared experiences of female artists through categorys of migration, ritual, personal space, and their identity. Through performances and live music that serve as a form of connecting different cultures and blurring boundaries, create new shape of dragons without gender, class, race and with a spiritual essence, have created utopian, imaginary beings without religious affiliations.',
+      ],
+      role: ['Performance'],
+      people: {
+        crew: [
+          'Hyeonsu Jung',
+          'Siwei Li',
+          'Soli Jang',
+          'Lara Duymus',
+          'Raimonda Žiūkaitė',
+        ],
+        photo: 'Helena Zaida',
+      },
+      schedule: [
+        {
+          date: '16, 17 February 2024',
+          location: 'Ballhaus Ost, Berlin',
+        },
+      ],
+      images: Array.from(
+        { length: 8 },
+        (_, index) =>
+          `/images/Artwork&Performance/DenyingTheDragon/DTD${index + 1}.JPG`
+      ),
+    },
+    {
+      title: 'Embodied',
+      summary: 'What is embodied in me?',
+      category: 'performance, artwork',
+      content: ['Bachelor graduation work'],
+      role: ['Installation', 'Performance'],
+      people: {
+        crew: ['Soli Jang', 'Dongryoung Han'],
+        video: 'Hyunsung Choi',
+        photo: 'Xu Che',
+      },
+      schedule: [
+        {
+          date: '19, 20, 21 July 2024',
+          location: 'Universität der Künste Berlin, Berlin',
+        },
+      ],
+      images: Array.from(
+        { length: 6 },
+        (_, index) =>
+          `/images/Artwork&Performance/Embodied/Embodied${index + 1}.JPG`
+      ),
+    },
+    {
+      title: 'Forest love : Meeting once',
+      summary: 'How and where can we connect?',
+      category: 'stage, performance',
+      content: [
+        'Part of the Room to expand Festival - UdK. A performance about the process of creating connections between human and non-human beings.',
+      ],
+      role: ['Set Design', 'Performance'],
+      people: {
+        crew: [
+          'Julia',
+          'Lea',
+          'Anastasia',
+          'Cécile',
+          'Ash',
+          'Lea Lenk',
+          'Ash Spit',
+          'Julia Kafizova',
+          'Soli Jang',
+          'Cécile Boos',
+          'Anastasia Putsykina',
+        ],
+        photo: 'Helena Zaida',
+      },
+      schedule: [
+        {
+          date: '16 February 2024',
+          location: 'Uferstudios, Berlin',
+        },
+      ],
+    },
+  ],
+
+  stage: [
+    {
+      title: 'Forest love : Meeting once',
+      summary: 'How and where can we connect?',
+      category: 'stage, performance',
+      content: [
+        'Part of the Room to expand Festival - UdK.',
+        'A performance about the process of creating connections between human and non-human beings.',
+      ],
+      role: ['Set Design', 'Performance'],
+      people: {
+        crew: [
+          'Julia',
+          'Lea',
+          'Anastasia',
+          'Cécile',
+          'Ash',
+          'Lea Lenk',
+          'Ash Spit',
+          'Julia Kafizova',
+          'Soli Jang',
+          'Cécile Boos',
+          'Anastasia Putsykina',
+        ],
+        photo: 'Helena Zaida',
+      },
+      schedule: [
+        {
+          date: '16 February 2024',
+          location: 'Uferstudios, Berlin',
+        },
+      ],
+    },
+    {
+      title: 'Digital Nomads',
+      summary: 'The stories of digital nomads.',
+      category: 'stage',
+      content: [
+        '"Digital Nomad" is a piece that was inspired by the stories of digital nomads, individuals who work remotely while globetrotting to UNESCO locations. The narrative-driven choreography explored the desires and challenges of these people, offering a portrayal of their way of life through movement and music.',
+      ],
+      role: ['Stage design'],
+      people: {
+        crew: [
+          'Holger Benedict Seidelin',
+          'Raimonda Žiūkaitė',
+          'Tamae Yoneda',
+          'Hannah Stein',
+          'Fotis Charonis',
+          'Soli Jang',
+          'Lucia Leibnitz',
+          'Joana Pratschke',
+          'Vincent Carter',
+        ],
+        video: 'Holger Benedict Seidelin',
+        photo: 'Minjoo Yoo',
+      },
+      schedule: [
+        {
+          date: '10, 11 November 2023',
+          location: 'Hans Eisler Studio Saal, Berlin',
+        },
+      ],
+    },
+    {
+      title: 'Musical 21:58',
+      summary:
+        '21:53 - Zu früh um schlafen zu gehen, zu spät um wen anzurufen.',
+      category: 'stage',
+      content: [
+        'Werkstattinszenierung unter Verwendung von Auszügen aus ALLES CASUAL von Elisabeth Pape.',
+        'Ein Projekt des Studiengangs Musical/Show in Kooperation mit den Studiengängen Bühnen-und Kostümbild.',
+      ],
+      role: ['Bühnenbild'],
+      people: {
+        crew: [
+          'Tatonka-Danae Brunner',
+          'Johanna Eid',
+          'Charlotte Fischer',
+          'Olivia Goga',
+          'Teresa Hank-Gómez',
+          'Lara Korner',
+          'Samantha Mayer',
+          'Melina Niccum',
+          'Insa Ommen',
+          'Alicia Reuß',
+          'Mathias Noack',
+          'Damian Omansen',
+          'Bart de Clercq',
+          'Lara Duymus',
+          'Jeanot Kempf',
+          'Soli Jang',
+          'Miriam Damm',
+          'Tobias Blinzler',
+          'Anastasia Troska',
+          'Karla Romero Vargas',
+        ],
+        photo: 'Daniel Nartschick',
+      },
+      schedule: [
+        {
+          date: '10, 11, 12, 17, 18, 19 May 2024',
+          location: 'UNI.T Theater, Berlin',
+        },
+      ],
+    },
+    {
+      title: 'The Rich Family',
+      summary: 'Was macht einen tollen Sommer aus?',
+      category: 'stage',
+      content: [
+        'Produktionsfirmen.',
+        'Deutsche Film- und Fernsehakademie Berlin (DFFB).',
+      ],
+      role: ['Production Design(Szenenbild)'],
+      people: {
+        regie: ['Antonia Walther'],
+        schauspieler: [
+          'Rahel Savoldelli',
+          'Frederik von Lüttichau',
+          'Josephine Schumann',
+          'Tomas Kutinjač',
+          'Ares Kloß',
+          'Alma Löhr',
+          'Smilla Löh',
+          'Sina Seiler',
+          'Sylvia Schwarz',
+        ],
+        crew: [
+          'Hannah Marie Lorenz',
+          'Sven Krause',
+          'Kevin Laminit',
+          'Sven Krause',
+          'Soli Jang',
+          'Geoffrey LaRue',
+          'Janga Berk',
+          'Sven Krause',
+          'Antonia Walther',
+          'Kilian Zillessen',
+          'Anouk Lünterbusch',
+          'Sven Krause',
+          'Chun Mei Tan',
+          'Kevin Mehmke',
+        ],
+      },
+      schedule: [
+        {
+          date: '18 August 2023 - 31 August 2023',
+          location: 'Berlin, Deutschland',
+        },
+      ],
+    },
+    {
+      title: 'WUW',
+      summary: 'WIND UND WAND',
+      category: 'stage, costume',
+      content: [
+        'WUW is an abbreviation for WIND UND WAND (WIND AND WALL) and describes a 30-minute dance performance for young audiences aged 10+. The metaphors of the wind and the wall symbolize the ambivalent relationship between children and their parents and the gap between their wishes and reality. They also stand for intertwined thoughts and emotions and unclear interim periods. No longer a child and not yet a teenager - The time before puberty is both a formative and challenging phase. At the same time, it is a playful journey with many surprises, adventures, parties and unexpected transformations. The wind has a very flexible quality: it can be a violent storm or a gentle, pleasant breeze, and it can quickly change its mood – just like young people. Inspired by how the wind changes shape, ZINADA explores creative ways to overcome walls in WUW. During the rehearsal and development phase of the piece, the artists conducted interviews with the target group and held workshops on topics that concern children of this age group. The work with the target group was not just inspiring but also integral to the development of the movement material.',
+      ],
+      role: ['Costumes', 'Wall Design'],
+      people: {
+        crew: [
+          'Jin Lee',
+          'ZINADA (Jin Lee & Jihun Choi)',
+          'Benny Omerzell',
+          'Soli Jang',
+          'Jihun Choi',
+        ],
+        production: [
+          'Fokus Tanz in cooperation with HochX Theater und Live Art, München',
+        ],
+        photo: 'Christina Gerg',
+      },
+      schedule: [
+        {
+          date: '22, 23 October 2024',
+          location: 'Dante-Gymnasium and Klenze-Gymnasium in Munich',
+        },
+        {
+          date: '17, 18 January 2025',
+          location: 'Hochx Theater & Live music, Munich, Germany',
+        },
+        {
+          date: '17, 19 November 2024',
+          location: 'Fabrik Potsdam, Potsdam',
+        },
+        {
+          date: '18, 19 January 2025',
+          location: 'HochX Theater, Munich',
+        },
+        {
+          date: '09, 11 April 2025',
+          location: 'K3 Hamburg, Hamburg',
+        },
+        {
+          date: '15, 17 May 2025',
+          location: 'Potsdamer Tanztage, Potsdam',
+        },
+        {
+          date: '17, 18 June 2025',
+          location: 'Hellerau, Dresden',
+        },
+      ],
+    },
+  ],
+
+  costume: [
+    {
+      title: 'HUABUN',
+      summary: '"Potted plant" in Korean.',
+      category: 'costume',
+      content: [
+        'HUABUN ("potted plant" in Korean) is a 40-minute performance that deals with the unfamiliarity of time and space. It establishes connections between people and plants and the constant encounter with a different environment. HUABUN explores a movement language inspired by plants that scan their surroundings with 20 senses. ZINADA transforms their experience of relocation into a poetic performance.',
+      ],
+      role: ['Costume'],
+      people: {
+        crew: [
+          'ZINADA (Jin Lee & Jihun Choi)',
+          'Jin Lee',
+          'Benny Omerzell',
+          'Jin Young Won',
+          'Rainer Ludwig',
+          'Soli Jang',
+          'Laura Manz',
+          'Simone Lutz',
+        ],
+      },
+      schedule: [
+        {
+          date: '30 March, 01 April 2023',
+          location: 'HochX theater, Munich',
+        },
+      ],
+    },
+    {
+      title: 'WUW',
+      summary: 'WIND UND WAND',
+      category: 'stage, costume',
+      content: [
+        'WUW is an abbreviation for WIND UND WAND (WIND AND WALL) and describes a 30-minute dance performance for young audiences aged 10+. The metaphors of the wind and the wall symbolize the ambivalent relationship between children and their parents and the gap between their wishes and reality. They also stand for intertwined thoughts and emotions and unclear interim periods. No longer a child and not yet a teenager - The time before puberty is both a formative and challenging phase. At the same time, it is a playful journey with many surprises, adventures, parties and unexpected transformations. The wind has a very flexible quality: it can be a violent storm or a gentle, pleasant breeze, and it can quickly change its mood – just like young people. Inspired by how the wind changes shape, ZINADA explores creative ways to overcome walls in WUW. During the rehearsal and development phase of the piece, the artists conducted interviews with the target group and held workshops on topics that concern children of this age group. The work with the target group was not just inspiring but also integral to the development of the movement material.',
+      ],
+      role: ['Costumes', 'Wall Design'],
+      people: {
+        crew: [
+          'Jin Lee',
+          'ZINADA (Jin Lee & Jihun Choi)',
+          'Benny Omerzell',
+          'Soli Jang',
+          'Jihun Choi',
+        ],
+        production: [
+          'Fokus Tanz in cooperation with HochX Theater und Live Art, München',
+        ],
+        photo: 'Christina Gerg',
+      },
+      schedule: [
+        {
+          date: '22, 23 October 2024',
+          location: 'Dante-Gymnasium and Klenze-Gymnasium in Munich',
+        },
+        {
+          date: '17, 18 January 2025',
+          location: 'Hochx Theater & Live music, Munich, Germany',
+        },
+        {
+          date: '17, 19 November 2024',
+          location: 'Fabrik Potsdam, Potsdam',
+        },
+        {
+          date: '18, 19 January 2025',
+          location: 'HochX Theater, Munich',
+        },
+        {
+          date: '09, 11 April 2025',
+          location: 'K3 Hamburg, Hamburg',
+        },
+        {
+          date: '15, 17 May 2025',
+          location: 'Potsdamer Tanztage, Potsdam',
+        },
+        {
+          date: '17, 18 June 2025',
+          location: 'Hellerau, Dresden',
+        },
+      ],
+    },
+  ],
+};
