435ba0c814fc6cfbeafa0e4e125fa9e2c2fca904
SongSang <941227_@naver.com>
2025-02-08 19:41:51 +0900

    ✨ : nav 현재 메뉴 상태 보기 기능 추가

--- components/NavBar/Menu/NavMenu.tsx
+++ components/NavBar/Menu/NavMenu.tsx
@@ -1,16 +1,64 @@
-import { useState } from 'react';
 import Link from 'next/link';
 import styles from './NavMenu.module.scss';
 import classNames from 'classnames/bind';
+import { useRouter } from 'next/router';
+import Lottie from 'react-lottie-player';
+import LineLottie from '@/public/lottie/home-line-animation.json';
 
 const cx = classNames.bind(styles);
 
 export default function NavMenu() {
+  const router = useRouter();
+  const { pathname } = router;
+
+  const currentMenu = pathname.split('/')[1];
+
   return (
     <div className={cx('navMenu-container')}>
-      <Link href="/works">projects</Link>
-      <Link href="/introduction">about</Link>
-      <Link href="/contact">contact</Link>
+      <Link
+        href="/projects"
+        className={cx('menu', { highlight: currentMenu === 'projects' })}
+      >
+        projects
+        {currentMenu === 'projects' && (
+          <Lottie
+            play={true}
+            loop={false}
+            animationData={LineLottie}
+            className={cx('line-lottie')}
+          />
+        )}
+      </Link>
+      <Link
+        href="/about"
+        className={cx('menu', { highlight: currentMenu === 'about' })}
+      >
+        about
+        {currentMenu === 'about' && (
+          <Lottie
+            play={true}
+            loop={false}
+            animationData={LineLottie}
+            className={cx('line-lottie')}
+            speed={1.5}
+          />
+        )}
+      </Link>
+
+      <Link
+        href="/contact"
+        className={cx('menu', { highlight: currentMenu === 'contact' })}
+      >
+        contact
+        {currentMenu === 'contact' && (
+          <Lottie
+            play={true}
+            loop={false}
+            animationData={LineLottie}
+            className={cx('line-lottie')}
+          />
+        )}
+      </Link>
     </div>
   );
 }
