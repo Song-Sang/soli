0ea4e6f2bb7780c8134cc109a772e854aafb9f96
SongSang <941227_@naver.com>
2025-03-04 20:01:25 +0900

    💄 : 반응형 UI 개선

--- components/WorksPage/SubTopic/SubTopic.tsx
+++ components/WorksPage/SubTopic/SubTopic.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import styles from './SubTopic.module.scss';
 import classNames from 'classnames/bind';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
@@ -14,10 +15,17 @@ interface SubTopicProps {
 export default function SubTopic({ title, category, summary }: SubTopicProps) {
   const UrlCategory = category.split(',')[0].trim();
 
+  const [selectTopic, setSelectTopic] = useState('');
+
+  const handleSubTopicClick = (subTopic: string) => {
+    setSelectTopic(subTopic);
+  };
+
   return (
     <Link
       href={`/projects/${UrlCategory}/${title}`}
-      className={cx('topic-wrapper')}
+      className={cx('topic-wrapper', { select: selectTopic })}
+      onClick={() => handleSubTopicClick(title)}
     >
       <div className={cx('title')}>{title}</div>
       <div className={cx('topic-details')}>
